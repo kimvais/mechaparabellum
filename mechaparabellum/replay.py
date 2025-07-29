b3b72894902b9f3a3aedc7efe017606e482ad774
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2025-07-29 05:28:20 +0300

    Add more constants.

--- mechaparabellum/replay.py
+++ mechaparabellum/replay.py
@@ -1,13 +1,19 @@
+import logging
+
 import fire
 from lxml import etree
 from xml.etree import ElementTree as ET
 import pathlib
 import tempfile
 
-from mechaparabellum.units import Contraption, \
+from mechaparabellum.units import CommanderSkill, \
+    Contraption, \
+    Item, \
     Reinforcement, \
-    Unit
-
+    Tower, \
+    TowerTech, \
+    Unit, \
+    UnitTech
 
 
 DIR = pathlib.Path('C:/Program Files (x86)/Steam/steamapps/common/Mechabellum/ProjectDatas/Replay')
@@ -48,81 +54,105 @@ def get_unit(action):
     uid = action.find('UID').text
     return Unit(int(uid))
 
-def parse(path):
-    if not isinstance(path, pathlib.Path):
-        path = pathlib.Path(path)
-    root = load(path)
-    player_record = find_correct_pr(root.find('playerRecords'))
-    for round in player_record.find('playerRoundRecords'):
-        print(f'\n\n --- Round: {round.find("round").text}')
-        unlocked_units = round.xpath('playerData/shop/unlockedUnits/int')
-        actions = round.xpath('actionRecords/MatchActionData')
-        for unit in unlocked_units:
-            print(f' Available: {Unit(int(unit.text))}')
-        for action in actions:
-            act_name = action.attrib.values()[0]
-            try:
-                match act_name:
-                    case 'PAD_ChooseAdvanceTeam':
-                        uid = action.find('ID').text
-                        idx = action.find('Index').text
-                        print(f'Choose starting setup: #{idx}: {uid}')
-                    case 'PAD_UpgradeTechnology':
-                        unit = get_unit(action)
-                        tech  = int(action.find('TechID').text)
-                        print(f'Upgrade technology: {unit} - {tech}')
-                    case 'PAD_ActiveEnergyTowerSkill':
-                        skill = int(action.find('SkillID').text)
-                        print(f'Active energy tower skill: {skill}')
-                    case 'PAD_BuyUnit':
-                        uid = get_unit(action)
-                        print(f'Buy unit: {uid}')
-                    case 'PAD_UnlockUnit':
-                        uid = get_unit(action)
-                        print(f'Unlock unit: {uid}')
-                    case 'PAD_UpgradeUnit':
-                        uid = get_unit(action)
-                        idx = action.find('UIDX').text
-                        print(f'Upgrade unit: {uid} #{idx}')
-                    case 'PAD_FinishDeploy':
-                        print('Finish deploy')
-                    case 'PAD_Undo':
-                        print('Undo')
-                    case 'PAD_ReleaseCommanderSkill':
-                        skill = int(action.find('ID').text)
-                        index = int(action.find('SkillIndex').text)
-                        print(f'Release commander skill: {skill}')
-                    case 'PAD_MoveUnit':
-                        moves = action.xpath('moveUnitDatas/MoveUnitData')
-                        for data in moves:
-                            x = data.find('position').find('x').text
-                            y = data.find('position').find('y').text
-                            uid = Unit(int(data.find('unitID').text))
-                            index = data.find('unitIndex').text
-                            print(f'Move unit: {uid} #{index} x: {x} y: {y}')
-                    case 'PAD_ChooseReinforceItem':
-                        idx = action.find('Index').text
-                        uid = action.find('ID').text
-                        item = Reinforcement(int(uid))
-                        print(f'Choose reinforcement: {item} ({idx})')
-                    case 'PAD_UseEquipment':
-                        uid = action.find('EquipmentID').text
-                        idx = action.find('UnitIndex').text
-                        print(f'Use equipment: {uid} on unit #{idx}')
-                    case 'PAD_ActiveBlueprint':
-                        uid = action.find('ID').text
-                        print(f'Activate spell: {uid}')
-                    case 'PAD_GiveUp':
-                        print('Surrender')
-                    case 'PAD_ReleaseContraption':
-                        uid = int(action.find('ContraptionID').text)
-                        item = Contraption(int(uid))
-                        print(f'Release contraption: {item}')
-                    case _:
-                        print(to_str(action))
-            except AttributeError:
-                print(to_str(action))
+class CLI:
+    @staticmethod
+    def parse(path):
+        if not isinstance(path, pathlib.Path):
+            path = pathlib.Path(path)
+        root = load(path)
+        player_record = find_correct_pr(root.find('playerRecords'))
+        for round in player_record.find('playerRoundRecords'):
+            print(f'\n\n --- Round: {round.find("round").text}')
+            unlocked_units = round.xpath('playerData/shop/unlockedUnits/int')
+            actions = round.xpath('actionRecords/MatchActionData')
+            for unit in unlocked_units:
+                print(f' Available: {Unit(int(unit.text))}')
+            for action in actions:
+                act_name = action.attrib.values()[0]
+                try:
+                    match act_name:
+                        case 'PAD_ChooseAdvanceTeam':
+                            uid = action.find('ID').text
+                            idx = action.find('Index').text
+                            print(f'Choose starting setup: #{idx}: {uid}')
+                        case 'PAD_UpgradeTechnology':
+                            unit = get_unit(action)
+                            tech, teched_unit  = UnitTech.parse(action.find('TechID').text)
+                            assert unit == teched_unit, (tech, unit, to_str(action))
+                            print(f'Upgrade technology: {unit} - {tech}')
+                        case 'PAD_ActiveEnergyTowerSkill':
+                            skill = TowerTech(int(action.find('SkillID').text))
+                            print(f'Active energy tower skill: {skill}')
+                        case 'PAD_BuyUnit':
+                            uid = get_unit(action)
+                            print(f'Buy unit: {uid}')
+                        case 'PAD_UnlockUnit':
+                            uid = get_unit(action)
+                            print(f'Unlock unit: {uid}')
+                        case 'PAD_UpgradeUnit':
+                            uid = get_unit(action)
+                            idx = action.find('UIDX').text
+                            print(f'Upgrade unit: {uid} #{idx}')
+                        case 'PAD_FinishDeploy':
+                            print('Finish deploy')
+                        case 'PAD_Undo':
+                            print('Undo')
+                        case 'PAD_ReleaseCommanderSkill':
+                            skill_id = int(action.find('ID').text)
+                            skill = CommanderSkill(skill_id)
+                            index = int(action.find('SkillIndex').text)
+                            print(f'Release commander skill: {skill}')
+                        case 'PAD_MoveUnit':
+                            moves = action.xpath('moveUnitDatas/MoveUnitData')
+                            for data in moves:
+                                x = data.find('position').find('x').text
+                                y = data.find('position').find('y').text
+                                uid = Unit(int(data.find('unitID').text))
+                                index = data.find('unitIndex').text
+                                print(f'Move unit: {uid} #{index} x: {x} y: {y}')
+                        case 'PAD_ChooseReinforceItem':
+                            idx = action.find('Index').text
+                            uid = action.find('ID').text
+                            item = Reinforcement.parse(uid)
+                            print(f'Choose reinforcement: {item} ({idx})')
+                        case 'PAD_UseEquipment':
+                            uid = action.find('EquipmentID').text
+                            item = Item(int(uid))
+                            idx = action.find('UnitIndex').text
+                            print(f'Use equipment: {item} on unit #{idx}')
+                        case 'PAD_ActiveBlueprint':
+                            uid = action.find('ID').text
+                            print(f'Activate spell: {uid}')
+                        case 'PAD_GiveUp':
+                            print('Surrender')
+                        case 'PAD_ReleaseContraption':
+                            uid = int(action.find('ContraptionID').text)
+                            item = Contraption(int(uid))
+                            print(f'Release contraption: {item}')
+                        case 'PAD_StrengthenTower':
+                            tower_idx = action.find('Index').text
+                            tower = Tower(int(tower_idx))
+                            print(f'Strengthen tower: {tower}')
+                        case 'PAD_CancelReleaseCommanderSkill':
+                            skill_id = int(action.find('ID').text)
+                            skill = CommanderSkill(skill_id)
+                            print(f'Cancel release commander skill {skill}')
+                        case _:
+                            print(to_str(action))
+                            raise Exception(f'Unknown action: {action}')
+                except AttributeError as e:
+                    logging.exception(e)
+                    print(to_str(action))
+                except ValueError:
+                    print(to_str(action))
+                    raise
+
+    def parse_all(self):
+        for path in DIR.glob('*.grbr'):
+            print(f'Parsing {path.name}')
+            self.parse(path)
+
 
 
 if __name__ == '__main__':
-    fire.Fire(parse)
+    fire.Fire(CLI)
--- mechaparabellum/units.py
+++ mechaparabellum/units.py
@@ -1,5 +1,7 @@
 import enum
 
+from asttokens.util import is_starred
+
 """
     18: {1},
     26: {1},
@@ -37,44 +39,87 @@ class Unit(NamedEnum):
     PHANTOM_RAY = 25
     # 100 + 200: Hacker
     # 50 + 300: Wraith, Scorpion, Farseer
+    FARSEER = 26
     WRAITH = 18
     # The following must be 1, 3, 14, 17, 19, 23, 26, and 27
     # 200 + 400: Vulcan, Sandworm, Raiden
+    RAIDEN = 27
     FORTRESS = 1
+    SANDWORM = 23
     MELTING_POINT = 4
-    # 200 + 500: Overlord
+    # 200 + 500:
+    OVERLORD = 11
     # 200 + 800: War Factory, Mountain
     ABYSS = 29
     # 200 drops:
     TYPHOON = 22
     FIRE_BADGER = 20
 
+class Item(NamedEnum):
+    ABSORPTION_MODULE = 1309001 # 150
+    AMPLIFYING_CORE = 13030007 # 150
+    HASTE_MODULE = 13030005
+    HEAVY_ARMOR = 13030002
+    IMPROVED_FIREPOWER_CONTROL_SYSTEM = 13030003  # ???
+    LASER_SIGHTS = 13030001
+    NANO_REPAIR_KIT = 13020001
+    SMALL_AMPLIFYING_CORE = 13030009
+    STEEL_BALL_PRODUCTION_LINE = 1306003
+
 class Reinforcement(NamedEnum):
     # Units = 10.XRUU, where X is amount, R is rank and UU is unit ID . is probably price?
     # 0000 in string format = spell
     # Unit modifiers = 3UU??
-    # Items = 13.+
-    ELECTROMAGNETIC_IMPACT = 200001
-    HEAVY_ARMOR = 1303000
-    INCENDIARY_BOMB = 100002
-    LIGHTNING_STORM = 300005
-    ORBITAL_BOMBARDMENT = 300003
-    MASS_PRODUCED_SLEDGEHAMMER = 31301   # Free
+    # Items = 130+
+    ADDITIONAL_DEPLOYMENT_SLOT = 10004
+    ADVANDED_DEFENSIVE_TACTICS = 20001
     ASSAULT_STORMCALLER = 31201  #  Free
-    MOBILE_BEACON = 1500002
-    RANK_1_FIRE_BADGER_X3 = 1043120
-    RANK_2_FIRE_BADGER_X2 = 1042220  # 50
-    RANK_2_PHANTOM_RAY_X1 = 1062225
-    RANK_3_TYPHOON_X1 = 1061322
-    RANK_3_WASP_X1 = 103136
-    RANK_1_FORTRESS_X2 = 107211  # 150
+    EFFICIENT_TECH_RESEARCH = 20003
+    ELITE_CRAWLER = 31002
+    ELITE_HACKER = 31403
+    FORTIFIED_OVERLORD = 31101
+    EXTENDED_RANGE_SABERTOOTH = 32101
+    IMPROVED_MELTING_POINT = 30402 #
+    MASS_PRODUCED_SLEDGEHAMMER = 31301   # Free
+    MASS_PRODUCED_RHINO = 30501
+    ORBITAL_BOMBARDMENT = 300003
+    QUICK_COOLDOWN = 10001
+    QUICK_TELEPORT = 10009
     SUBSIDIZED_CRAWLER = 31001
+    SUBSIDIZED_STEEL_BALL = 30801
+    SUPER_SUPPLY_ENHANCEMENT = 10003
     VULCANS_DESCENT = 1200005
-    AMPLIFYING_CORE = 13030007 # 150
-    ABSORPTION_MODULE = 1309001 # 150
+    WASP_SWARM_MAYBE = 13030001 # This is false.
+    XXX = 20006
+
+    @classmethod
+    def parse(cls, id_: str):
+        try:
+            return cls(int(id_))
+        except ValueError:
+            if len(id_) == 5:
+                unit = Unit(int(id_[1:3]))
+                return f'Unknown Unit buff {id_} for {unit}'
+            else:
+                match int(id_[:3]):
+                    case 101 | 102 | 103 | 104 | 105 | 106 | 107 | 108 | 109:  # ?
+                        rank = int(id_[4])
+                        amount = int(id_[3])
+                        unit = Unit(int(id_[5:]))
+                        return f'{amount} x Rank {rank} {unit}'
+                    case 130:
+                        return Item(int(id_))
+                    case _:
+                        if '0000' in id_:
+                            try:
+                                return CommanderSkill(int(id_))
+                            except ValueError:
+                                return f'{id_} is an unknown battlefield power.'
+                        else:
+                            raise ValueError(f'Unknown reinforcement id: {id_}')
 
 
-class TowerTechs(NamedEnum):
+class TowerTech(NamedEnum):
     RAPID_RESUPPLY = 1
     HIGH_MOBILITY = 2  # ?
     MASS_RECRUITMENT = 3
@@ -83,19 +128,23 @@ class TowerTechs(NamedEnum):
 
 
 class Specialist(NamedEnum):
-    TRAINING = 9894  # Arclights and Sabretooths
-    RHINO = 9878  # Crawlers and StormCallers
+    TRAINING = 9894
+    RHINO = 9878
+    AMPLIFY = 9897  # And 9884?
+    FORTIFIED = 9896
+    TYPHOON = 9871  # 9875
+    MARKSMAN = 9876
+    COST_CONTROL = 9879  # Typhoon?!
+    SABERTOOTH = 9893
+
+
+
+class Tower(NamedEnum):
+    RESEARCH_CENTER = 0  # ?
+    COMMAND_CENTER = 1
+
 
 class UnitTech(NamedEnum):
-    # Range = 102 + UID
-    FIRE_BADGER_RANGE = 10220
-    PHANTOM_RAY_RANGE = 10225
-    MUSTANG_RANGE = 10207
-    SNIPER_RANGE = 10202
-    PHANTOM_RAY_OIL = 11025   # Oil = 110
-    MUSTANG_ANTI_MISSILE = 3307
-    SABER_RANGE = 10221
-
     # Mustang / 3307
     ANTI_MISSILE = 33
     # Fire badger / 10220, Mustang / 10207, Sniper / 10202, Sabertooth / 10221, Phantom Ray / 10225
@@ -104,8 +153,57 @@ class UnitTech(NamedEnum):
     WRECKAGE_RECYCLING = 23
     POWER_ARMOR = 25
     FINAL_BLITZ = 28
-    # Phantom ray / ...25
+    # Phantom ray / 11025
     OIL_BOMB = 110
+    # Steel Ball / 2408, 1308
+    FORTIFIED_TARGET_LOCK = 24
+    MECHANICAL_DIVISION = 13
+    # Mustang / 3207
+    AERIAL_SPECIALIZATION = 32
+    # Fortress / 1105
+    ANTI_AIR_BARRAGE = 11
+    # Fortress / 1001
+    BARRIER = 10
+    # Sabretooth / 10321
+    FIELD_MAINTENANCE = 103
+    # Farseer / 1826
+    ELECTROMAGNETIC_EXPLOSION = 18
+    # Typhoon / 3022
+    MECHANICAL_RAGE_TYPHOON = 30
+    # Crawler / 10510
+    MECHANICAL_RAGE_CRAWLER = 105
+    # Melting point / 1107 (Sic!)
+    ENERGY_DIFFRACTION = 1107
+    # Phoenix / 2916
+    QUANTUM_REASSEMBLY = 29
+    # Hound / 4228
+    FIRE_EXTINGUISHER = 42
+    CHARGED_SHOT = 109
+    # Crawler / 2610
+    SUBTERRANEAN_BLITZ = 26
+    # Melting Point / 304
+    ENERGY_ABSORPTION = 3
+
+    @classmethod
+    def parse(cls, id_: str):
+        match id_:
+            case '1105':
+                tech = cls.ANTI_AIR_BARRAGE
+                unit = Unit.FORTRESS
+            case '1001':
+                tech = cls.BARRIER
+                unit = Unit.FORTRESS
+            case '905':
+                tech = cls.FIELD_MAINTENANCE
+                unit = Unit.RHINO
+            case '1107':
+                tech = cls.ENERGY_DIFFRACTION
+                unit = Unit.MELTING_POINT
+            case _:
+                tech = cls(int(id_[:-2]))
+                unit = Unit(int(id_[-2:]))
+
+        return tech, unit
 
 class CommanderSkill(NamedEnum):
     INTENSIVE_TRAINING = 1100001
@@ -113,6 +211,12 @@ class CommanderSkill(NamedEnum):
     STICKY_OIL = 400002
     MOBILE_BEACON = 1500002
     MASS_RECRUITMENT = 1100001
+    ELECTROMAGNETIC_IMPACT = 200001
+    SHIELD_AIRDROP = 800001
+    ACID_BLAST = 500002
+    PHOTON_EMISSION = 200003
+    XXX = 300006
+    XXX_SHIELD_AIRDROP = 1000001 # ?!
 
 class BluePrint(NamedEnum):
     STICKY_OIL = 1
@@ -122,4 +226,6 @@ class BluePrint(NamedEnum):
     DEFENCE_ENHANCEMENT = 5
 
 class Contraption(NamedEnum):
-    SHIELD_GENERATOR = 20001
+    SHIELD_GENERATOR = 10001
+    SENTRY_MISSILE = 20001  # ?
+    MISSILE_INTERCEPTOR = 30001  # ?
